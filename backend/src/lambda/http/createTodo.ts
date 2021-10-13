7e398c77d178644ae6eb6fc17b52682bbf86353a
Dieter Jackson <jacksondieter@gmail.com>
2021-10-13 18:40:49 +0200

    feat: Add createTodo

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,21 +1,29 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
 import * as middy from 'middy'
-import { cors } from 'middy/middlewares'
+import { cors, httpErrorHandler } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { getUserId } from '../utils';
-import { createTodo } from '../../businessLogic/todos'
+import { getUserId, Responses } from '../utils';
+import { createTodo } from '../../helpers/todos'
+import {createLogger} from '../../utils/logger'
+const logger = createLogger('createTodo')
 
-export const handler = middy(
+const createTodoHandler =
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    logger.info(`Event: ${event}`)
+    const userId = getUserId(event)
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
     // TODO: Implement creating a new TODO item
-
-    return undefined
-)
-
-handler.use(
-  cors({
-    credentials: true
-  })
-)
+    const todo = await createTodo(newTodo,userId)
+    if (!todo) {
+      return Responses._404({ message: 'Failed to create todo' })
+    }
+    return Responses._201({todo})
+  }
+export const handler = middy(createTodoHandler)
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
