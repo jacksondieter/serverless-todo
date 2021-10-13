26c940b52575feb8ebf1dbafc9df380ca93803b2
Dieter Jackson <jacksondieter@gmail.com>
2021-10-13 18:40:20 +0200

    feat: Add getTodo

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -4,19 +4,27 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 
-import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
-import { getUserId } from '../utils';
+import { getTodosForUser as getTodosForUser } from '../../helpers/todos'
+import { getUserId, Responses } from '../utils';
+import {createLogger} from '../../utils/logger'
+const logger = createLogger('getTodo')
 
-// TODO: Get all TODO items for a current user
-export const handler = middy(
+// TODO: Get all TODO items for a current user - DONE
+
+const getTodosHandler =
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    // Write your code here
-    const todos = '...'
+    logger.info(`Event: ${event}`)
+    const userId = getUserId(event)
+    const todos = await getTodosForUser(userId)
+    if (!todos) {
+      return Responses._404({ message: 'Failed to get todos' })
+    }
+    return Responses._200({ todos })
+  }
 
-    return undefined
-
-handler.use(
-  cors({
-    credentials: true
-  })
-)
+export const handler = middy(getTodosHandler)
+    .use(
+      cors({
+        credentials: true
+      })
+    )
