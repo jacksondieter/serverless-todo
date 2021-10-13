88d3bc24a87544a2a6290a83230055e7f7b19f1f
Dieter Jackson <jacksondieter@gmail.com>
2021-10-13 18:39:17 +0200

    feat: add Responses

--- backend/src/lambda/utils.ts
+++ backend/src/lambda/utils.ts
@@ -14,3 +14,32 @@ export function getUserId(event: APIGatewayProxyEvent): string {
 
   return parseUserId(jwtToken)
 }
+
+export const Responses = {
+  _DefineResponse(statusCode = 502, data = {}) {
+      return {
+          headers: {
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Methods': '*',
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Credentials': true
+          },
+          statusCode,
+          body: JSON.stringify(data),
+      };
+  },
+
+  _200(data = {}) {
+      return this._DefineResponse(200, data);
+  },
+  _201(data = {}) {
+    return this._DefineResponse(201, data);
+  },
+
+  _400(data = {}) {
+      return this._DefineResponse(400, data);
+  },
+  _404(data = {}) {
+      return this._DefineResponse(404, data);
+  },
+};
