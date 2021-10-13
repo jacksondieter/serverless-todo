90f29cb262a7ba53f10b281a03ff5f66962a5a83
Dieter Jackson <jacksondieter@gmail.com>
2021-10-13 18:41:05 +0200

    feat: Add deleteTodo

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -4,19 +4,24 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { deleteTodo } from '../../businessLogic/todos'
-import { getUserId } from '../utils'
+import { deleteTodo } from '../../helpers/todos'
+import { getUserId, Responses } from '../utils'
+import {createLogger} from '../../utils/logger'
+const logger = createLogger('updateTodo')
 
-export const handler = middy(
+const deleteTodoHandler =
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    logger.info(`Event: ${event}`)
+    const userId = getUserId(event)
     const todoId = event.pathParameters.todoId
-    // TODO: Remove a TODO item by id
-    
-    return undefined
+    // TODO: Remove a TODO item by id - DONE
+    const todo = await deleteTodo(todoId, userId)
+    if (!todo) {
+      return Responses._404({ message: 'Failed to delete todo' })
+    }
+    return Responses._200({ todo })
   }
-)
-
-handler
+export const handler = middy(deleteTodoHandler)
   .use(httpErrorHandler())
   .use(
     cors({
