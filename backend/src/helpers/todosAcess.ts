cb3b2efe82c0275ea73e6a56c3fa14ee5ca0abbc
Dieter Jackson <jacksondieter@gmail.com>
2021-10-14 15:10:22 +0200

    fix: Change update in db

--- backend/src/helpers/todosAcess.ts
+++ backend/src/helpers/todosAcess.ts
@@ -1,15 +1,12 @@
-import * as AWS from 'aws-sdk'
-// import * as AWSXRay from 'aws-xray-sdk'
+// import * as AWS from 'aws-sdk'
+import {XAWS as AWS}from './AWS'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 import { createLogger } from '../utils/logger'
 import { TodoItem } from '../models/TodoItem'
 import { TodoUpdate } from '../models/TodoUpdate';
 
-// const XAWS = AWSXRay.captureAWS(AWS)
-
 const logger = createLogger('TodosAccess')
 
-// TODO: Implement the dataLayer logic
 export class TodoAccess {
 
   constructor(
@@ -20,7 +17,7 @@ export class TodoAccess {
   }
 
   async getTodosForUser(userId): Promise<TodoItem[]> {
-      logger.info(`Getting all todos for user:${userId}`)
+    logger.info(`Getting all todos for user:${userId}`)
 
     const result = await this.docClient.query({
       TableName: this.todosTable,
@@ -33,6 +30,7 @@ export class TodoAccess {
   }
 
   async createTodo(todo: TodoItem): Promise<TodoItem> {
+    logger.info(`Create todos`)
     await this.docClient.put({
       TableName: this.todosTable,
       Item: todo
@@ -56,22 +54,22 @@ export class TodoAccess {
 
   }
   async updateTodo(todoId: string, userId:string, todoUpdate:TodoUpdate) {
+    logger.info(`Update todos for todo:${todoId}`)
     return await this.docClient.update({
         TableName: this.todosTable,
         Key: {
           todoId,
           userId
         },
-        UpdateExpression: 'set name = :name, dueDate = :dueDate, done = :done',
+        UpdateExpression: 'set done = :done',
         ExpressionAttributeValues: {
-            ":name": todoUpdate.name,
-            ":dueDate": todoUpdate.dueDate,
             ":done": todoUpdate.done
         }
     }).promise()
   }
 
   async deleteTodo(todoId: string, userId:string) {
+    logger.info(`Delete todos for todo:${todoId}`)
       return await this.docClient.delete({
           TableName: this.todosTable,
           Key: {
@@ -81,7 +79,8 @@ export class TodoAccess {
         }).promise()
   }  
 
-  async updateAttachmentUrl(todoId: string, userId:string, url:string) {
+  async updateAttachmentUrl(todoId: string, userId:string, imageId:string) {
+    logger.info(`Update url for todo:${todoId}`)
     return await this.docClient.update({
         TableName: this.todosTable,
         Key: {
@@ -90,20 +89,13 @@ export class TodoAccess {
         },
         UpdateExpression: 'set attachmentUrl = :url',
         ExpressionAttributeValues: {
-            ":url": url
+            ":url": imageId
         }
     }).promise()
   }
 }
   
 function createDynamoDBClient() {
-  if (process.env.IS_OFFLINE) {
-    console.log('Creating a local DynamoDB instance')
-    return new AWS.DynamoDB.DocumentClient({
-    region: 'localhost',
-    endpoint: 'http://localhost:8000'
-    })
-  }
-
+  // @ts-ignore: Unreachable code error
   return new AWS.DynamoDB.DocumentClient()
 }
